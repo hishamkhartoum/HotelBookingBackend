ecc10e16ef522c325d5aa5a4a60eb76d95c0ed94
Hisham Khartoum <hishamkhartoum@gmail.com>
2025-07-28 00:28:18 +0100

    Added Auth Controller

--- src/main/java/com/hadef/hotelbooking/controller/AuthenticationController.java
+++ src/main/java/com/hadef/hotelbooking/controller/AuthenticationController.java
@@ -10,6 +10,7 @@ import com.hadef.hotelbooking.service.AuthService;
 import com.hadef.hotelbooking.util.JwtUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
